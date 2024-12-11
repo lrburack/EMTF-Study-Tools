08f817b9b4d20376e3a0970592a329a73d56f004
Linden Burack <linden.burack@gmail.com>
2024-12-11 20:40:05 +0100

    implementation progress

--- Dataset/NewBend/TrackSelectors.py
+++ Dataset/NewBend/TrackSelectors.py
@@ -1,8 +1,13 @@
 from Dataset.Dataset import TrackSelector
 from Dataset.constants import *
+from Dataset.NewBend import NewBend_LUT
 
 class NewBendTrackSelector(TrackSelector):
+    # This is made only for modes which contain station 1.
     def __init__(self, mode: int, include_mode_15: bool = True, dR_match_max: float = 0.15):
+        if not get_station_presence(mode)[0]:
+            raise ValueError("The NewBendTrackSelector can only be used for modes with station 1")
+        
         super().__init__(mode, include_mode_15)
         self.dR_match_max = dR_match_max
 
@@ -14,10 +19,15 @@ class NewBendTrackSelector(TrackSelector):
         # Start by assuming that none of the tracks have a matching GEM hit
         has_GEM_match = np.zeros(len(good_tracks), dtype=bool)
 
+        # Candidate 
         GEM_hitrefs = np.where(np.array(event["EMTFNtuple"].emtfHit_type) == 3)
 
-        # Loop through each track which was identified as 'good' and impose that it has a matching GEM hit
+        # Loop through each track which was identified as 'good' and check if it has a matching GEM hit
         for i, track in enumerate(good_tracks):
+            # Remove station 1 ring 2 because this cant have a GEM match
+            if event["EMTFNtuple"].emtfTrack_ptLUT_st1_ring2[track]:
+                continue
+
             phi = np.abs(event["EMTFNtuple"].emtfTrack_phi[track]) * (np.pi / 180)
             eta = event["EMTFNtuple"].emtfTrack_eta[track]
 
